2d27961745a3d11a62cf7d9892ab7f082d823030
Luis Felipe Sanchez Sanchez <155125471+FelipeSanchezS@users.noreply.github.com>
2024-12-04 18:29:01 +0000

    Se establecion funcion startGame

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
-window.addEventListener('resize', startGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-function startGame() {
-  let canvasSize;
+
+
+function setCanvasSize(){
 
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
@@ -17,6 +18,12 @@ function startGame() {
   canvas.setAttribute('height', canvasSize);
   
   const elementsSize = canvasSize / 10;
+  startGame();
+
+}
+
+//Funcion de inicio de juego
+function startGame() {
 
   console.log({ canvasSize, elementsSize });
 
@@ -26,17 +33,4 @@ function startGame() {
   for (let i = 1; i <= 10; i++) {
     game.fillText(emojis['X'], elementsSize, elementsSize * i);
   }
-  
-  // window.innerHeight
-  // window.innerWidth
-  
-  // game.fillRect(0,50,100,100);
-  // game.clearRect(50,50,50,50);
-  // game.clearRect()
-  // game.clearRect(0,0,50,50);
-
-  // game.font = '25px Verdana'
-  // game.fillStyle = 'purple';
-  // game.textAlign = 'center';
-  // game.fillText('Platzi', 25, 25);
 }
